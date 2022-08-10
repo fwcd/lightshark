b346f42b8583bf5931b887b06d4263111cea3014
fwcd <fwcdmail@gmail.com>
2022-08-11 01:23:02 +0200

    Set up skeletal application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use log::Level;
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    simple_logger::init_with_level(Level::Info).unwrap();
+    
+    // TODO
+
+    Ok(())
 }
