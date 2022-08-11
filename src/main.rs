d3a998d9761286b957a1bdf877409fc08ef5b74a
fwcd <fwcdmail@gmail.com>
2022-08-11 02:01:56 +0200

    Implement proxy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+mod proxy;
+
 use anyhow::Result;
-use log::Level;
+use async_std::net::TcpListener;
+use clap::Parser;
+use log::{Level, info};
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// The WebSocket URL to proxy to.
+    url: String,
+    /// The host to listen on.
+    #[clap(short, long, default_value = "localhost")]
+    host: String,
+    /// The port to listen on.
+    #[clap(short, long, default_value_t = 9094)]
+    port: u16,
+}
 
 #[async_std::main]
 async fn main() -> Result<()> {
     simple_logger::init_with_level(Level::Info).unwrap();
     
-    // TODO
+    let args = Args::parse();
+
+    let listener = TcpListener::bind((args.host.as_str(), args.port)).await?;
+    info!("Listening on {}:{}...", args.host, args.port);
+
+    let (client_stream, client_addr) = listener.accept().await?;
+    info!("Connected to {}", client_addr);
+
+    proxy::proxy(client_stream, args.url.as_str()).await?;
 
     Ok(())
 }
---
+++ src/proxy.rs
@@ -0,0 +1,38 @@
+use anyhow::Result;
+use async_std::{net::TcpStream, task};
+use async_tungstenite::{async_std::connect_async, accept_async};
+use futures::{SinkExt, StreamExt};
+
+/// Proxies the traffic between the given client and the given server URL.
+pub(crate) async fn proxy(client_stream: TcpStream, server_url: &str) -> Result<()> {
+    let client_ws = accept_async(client_stream).await?;
+    let (server_ws, _) = connect_async(server_url).await?;
+
+    // Split connections so we can move them to their respective tasks
+    let (mut client_sink, mut client_stream) = client_ws.split();
+    let (mut server_sink, mut server_stream) = server_ws.split();
+
+    // Relay messages from server to client
+    let client_proxy = task::spawn::<_, Result<()>>(async move {
+        while let Some(msg_result) = server_stream.next().await {
+            let msg = msg_result?;
+            client_sink.send(msg).await?;
+        }
+        Ok(())
+    });
+
+    // Relay messages from client to server
+    let server_proxy = task::spawn::<_, Result<()>>(async move {
+        while let Some(msg_result) = client_stream.next().await {
+            let msg = msg_result?;
+            server_sink.send(msg).await?;
+        }
+        Ok(())
+    });
+
+    // Join client and server
+    client_proxy.await?;
+    server_proxy.await?;
+
+    Ok(())
+}
